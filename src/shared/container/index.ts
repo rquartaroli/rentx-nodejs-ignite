3a575893ac5bb20b3185d39b91b319149794f1c7
Rafael Quartaroli <rafaelquartaroli@hotmail.com>
2021-12-16 21:17:53 -0300

    Chapter 4 almost completed

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -8,6 +8,8 @@ import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpec
 import { SpecificationsRepository } from '../../modules/cars/infra/typeorm/repositories/SpecificationsRepository';
 import { ICarsRepository } from '../../modules/cars/repositories/ICarsRepository';
 import { CarsRepository } from '../../modules/cars/infra/typeorm/repositories/CarsRepository';
+import { ICarsImagesRepository } from '../../modules/cars/repositories/ICarsImagesRepository';
+import { CarsImagesRepository } from '../../modules/cars/infra/typeorm/repositories/CarsImagesRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -28,3 +30,9 @@ container.registerSingleton<ICarsRepository>(
   "CarsRepository",
   CarsRepository
 );
+
+container.registerSingleton<ICarsImagesRepository>(
+  "CarsImagesRepository",
+  CarsImagesRepository
+);
+
