5659c74daf6aee803223fb55f641390632fabd5d
Rafael Quartaroli <rafaelquartaroli@hotmail.com>
2021-12-17 14:37:56 -0300

    Chapter 4 completed

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import "../../shared/container/providers";
+
 import { IUsersRepository } from '../../modules/accounts/repositories/IUsersRepository';
 import { UsersRepository } from '../../modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
@@ -10,6 +12,8 @@ import { ICarsRepository } from '../../modules/cars/repositories/ICarsRepository
 import { CarsRepository } from '../../modules/cars/infra/typeorm/repositories/CarsRepository';
 import { ICarsImagesRepository } from '../../modules/cars/repositories/ICarsImagesRepository';
 import { CarsImagesRepository } from '../../modules/cars/infra/typeorm/repositories/CarsImagesRepository';
+import { IRentalsRepository } from '../../modules/rentals/repositories/IRentalsRepository';
+import { RentalsRepository } from '../../modules/rentals/infra/typeorm/repositories/RentalsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
@@ -34,5 +38,10 @@ container.registerSingleton<ICarsRepository>(
 container.registerSingleton<ICarsImagesRepository>(
   "CarsImagesRepository",
   CarsImagesRepository
+);
+
+container.registerSingleton<IRentalsRepository>(
+  "RentalsRepository",
+  RentalsRepository
 );
 
